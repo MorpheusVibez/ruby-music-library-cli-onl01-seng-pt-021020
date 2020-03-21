7d86b873f5bc33aa1866476c07874bdbaa1e4948
Andre Skeene <halodre24@gmail.com>
2020-03-21 19:35:21 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,35 @@
+class Genre
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    genre = new(name)
+    genre.save
+    genre
+  end
+
+  def songs
+    @songs
+  end
+
+  def artists
+    songs.collect(&:artist).uniq
+  end
+end
